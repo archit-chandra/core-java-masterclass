b4054771cd10c87e9ec17ff78cd101a3c9287b0c
Archit Chandra <send2archit.chandra.com>
2018-01-03 00:05:41 +0100

    Strings

---
+++ src/com/gemalto/string/Strings.java
@@ -0,0 +1,32 @@
+package com.gemalto.string;
+
+public class Strings {
+	public static void main(String[] args) {
+		
+		String myString = "This is a string";
+		System.out.println("myString = " + myString);
+		
+		// + works as a String appender
+		myString = myString +  " and this is appended string";
+		System.out.println("myString = " + myString);
+		
+		// adding unicode character also
+		myString = myString + " \u00A9 2015";
+		System.out.println("myString = " + myString);
+		
+		// adding string & number (it appends!!)
+		// operator overloading of '+'
+		String numberString = "250.55";
+		numberString = numberString + "49.55";
+		System.out.println("numberString = " + numberString);
+		
+		String lastString = "10";
+		int myInt = 50;
+		lastString = lastString + myInt;
+		System.out.println("lastString = " + lastString); //1050
+		
+		// same with double and other data types
+
+	}
+
+}
