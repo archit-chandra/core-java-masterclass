c5b7267f252eac161a31f1bbb7f207031ef1226a
Archit Chandra <send2archit.chandra.com>
2018-01-02 23:46:36 +0100

    Char & Boolean

---
+++ src/com/gemalto/variables/CharAndBoolean.java
@@ -0,0 +1,13 @@
+package com.gemalto.variables;
+
+public class CharAndBoolean {
+	
+	//only one alphabetical character or uni-code characeter
+	// width = 16 (2 byte)
+	// www.unicode-table.com
+	char myCharValue = 'A';
+	
+	// width = ?
+	boolean myBooleanValue = true;
+
+}
