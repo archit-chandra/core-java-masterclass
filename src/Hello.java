bb54066eb60e74b82428f9c1c57fe8629370a1fe
Archit Chandra <send2archit.chandra.com>
2018-01-02 22:49:00 +0100

    int

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,16 @@
 
 public class Hello {
 	public static void main(String[] args) {
+		
 		System.out.println("Hello, World!");
+		
+		int myFirstNumber = 5;
+		System.out.println("myFirstNumber = " + myFirstNumber);
+		int mySecondNumber = 5;
+		int sum = myFirstNumber + mySecondNumber;
+		System.out.println("The sum is = " + sum);
+		
+		int expression = (10 + 5) + (2 * 10);
+		System.out.println("expression = " + expression);
 	}
 }
