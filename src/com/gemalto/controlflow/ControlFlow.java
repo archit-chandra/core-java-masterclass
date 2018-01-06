f0450d854f33df3918cff23cb3e369903d55aceb
Archit Chandra <send2archit.chandra.com>
2018-01-06 01:37:12 +0100

    switch case

--- src/com/gemalto/controlflow/ControlFlow.java
+++ src/com/gemalto/controlflow/ControlFlow.java
@@ -4,19 +4,33 @@ public class ControlFlow {
     
     public static void main(String[] args) {
         
+        // if, else, else if
+        
         // switch case
+        // break are necessary to run each case only or will continue to run below cases
+        // too
+        // works with byte, int, char, short & strings(from JDK7)
+
+        int value = 4;
+        switch (value) {
+            case 1:
+                System.out.println("Value = 1");
+                break;
+            case 2:
+                System.out.println("Value = 2");
+                break;
+            
+            // valid statement for multiple cases
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Value could be multiple cases");
+                System.out.println("Value = " + value);
+                break;
+            default:
+                System.out.println("Value = neither 1 nor 2");
+        }
         
-        
-        
-        // for
-        
-        
-        
-        // while
-        
-        
-        
-        // doWhile
     }
     
 }
