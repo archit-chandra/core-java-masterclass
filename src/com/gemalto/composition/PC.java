9604bfe68e42df9395aed3f93e26742e1b2153a2
Archit Chandra <send2archit.chandra.com>
2018-01-06 06:39:58 +0100

    composition example

---
+++ src/com/gemalto/composition/PC.java
@@ -0,0 +1,12 @@
+package com.gemalto.composition;
+
+public class PC {
+    
+    // Composition
+    private Monitor monitor;
+    private Motherboard motherboard;
+    
+    // Constructors
+    // getters + setters
+    
+}
