c30cb370cd0df2ad9a8218a5c83b6d3b0dfd4703
Archit Chandra <send2archit.chandra.com>
2018-01-06 04:01:33 +0100

    inheritance example added

---
+++ src/com/gemalto/inheritance/Dog.java
@@ -0,0 +1,95 @@
+package com.gemalto.inheritance;
+
+public class Dog extends Animal {
+
+    // unique characteristics (states)
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+    
+    // common characteristics (states) => saved in super class
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        // calls the super class constructor
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+    
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
+    
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+    
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+    
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public void setTail(int tail) {
+        this.tail = tail;
+    }
+
+    public int getTeeth() {
+        return teeth;
+    }
+
+    public void setTeeth(int teeth) {
+        this.teeth = teeth;
+    }
+
+    public String getCoat() {
+        return coat;
+    }
+
+    public void setCoat(String coat) {
+        this.coat = coat;
+    }
+    
+}
---
+++ src/com/gemalto/inheritance/Fish.java
@@ -0,0 +1,59 @@
+package com.gemalto.inheritance;
+
+// incomplete class
+public class Fish extends Animal {
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+    
+    private void rest() {
+        
+    }
+    
+    private void moveMuscles() {
+        
+    }
+    
+    private void moveBackFin() {
+        
+    }
+    
+    private void swim(int speed) {
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
+    
+    public int getGills() {
+        return gills;
+    }
+
+    public void setGills(int gills) {
+        this.gills = gills;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    public int getFins() {
+        return fins;
+    }
+
+    public void setFins(int fins) {
+        this.fins = fins;
+    }
+
+}
