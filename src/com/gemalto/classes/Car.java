6d3bef5e0df910c3935b840537f4cdf602bfd061
Archit Chandra <send2archit.chandra.com>
2018-01-06 02:57:38 +0100

    constructors more info added

--- src/com/gemalto/classes/Car.java
+++ src/com/gemalto/classes/Car.java
@@ -6,24 +6,28 @@ package com.gemalto.classes;
 // object = instance of class
 
 public class Car {
-
+    
     // variables store state
     // methods store behavior
-
+    
     private int doors;
     private int wheels;
     private String model;
     private String engine;
     private String color;
-
+    
     // if you specify constructor using the fields, its safe to add default
     // constructor.
     // when no constructor is defined, Java provides this by itself
+    // can call one constructor from another constructor
+    // Advised : not to call other method or setters from the constructors
     public Car() {
-
+        this(5, 4, "high end model", "good engine", "blue");
     }
 
     // added the constructor using the fields
+    // to initialize the object that is being created
+    // called only once at the time when creating the object
     public Car(int doors, int wheels, String model, String engine, String color) {
         super();
         this.doors = doors;
@@ -32,30 +36,30 @@ public class Car {
         this.engine = engine;
         this.color = color;
     }
-
+    
     public int getDoors() {
         return doors;
     }
-
+    
     // getters & setters defined
     // Uses : can perform validations - see setModel()
-
+    
     public void setDoors(int doors) {
         this.doors = doors;
     }
-
+    
     public int getWheels() {
         return wheels;
     }
-
+    
     public void setWheels(int wheels) {
         this.wheels = wheels;
     }
-
+    
     public String getModel() {
         return model;
     }
-
+    
     public void setModel(String model) {
         String validModel = model.toLowerCase();
         if (validModel.equals("carrera") || validModel.equals("holden")) {
@@ -63,23 +67,23 @@ public class Car {
         } else {
             this.model = "unknown";
         }
-
+        
     }
-
+    
     public String getEngine() {
         return engine;
     }
-
+    
     public void setEngine(String engine) {
         this.engine = engine;
     }
-
+    
     public String getColor() {
         return color;
     }
-
+    
     public void setColor(String color) {
         this.color = color;
     }
-
+    
 }
--- src/com/gemalto/classes/Main.java
+++ src/com/gemalto/classes/Main.java
@@ -1,17 +1,23 @@
 package com.gemalto.classes;
 
 public class Main {
-
+    
     public static void main(String[] args) {
+        // initializing by default constructor
         Car porche = new Car(); // initializing with 'new'
-        Car holden = new Car();
-
+        
+        // initializing by user-defined constructor by fields
+        Car holden = new Car(5, 4, "high end model", "good engine", "black");
+        
         // very bad approach
         // class fields must always be PRIVATE
         // to access create public getters & setters
         /* porche.model = "Carrera"; */
         porche.setModel("Carrera"); // Right approach
         System.out.println("Model is : " + porche.getModel());
+        
+        System.out.println(holden.getColor());
+        
     }
-
+    
 }
