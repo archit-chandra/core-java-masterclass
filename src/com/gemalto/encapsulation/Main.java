096b0750488688ebe140eeb3de2ded14a2cd897d
Archit Chandra <send2archit.chandra.com>
2018-01-06 06:59:30 +0100

    encapsulation info added

---
+++ src/com/gemalto/encapsulation/Main.java
@@ -0,0 +1,21 @@
+package com.gemalto.encapsulation;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // private, public => encapsulation
+        
+        // BEST PRACTICES
+        // variables => private
+        // methods => public
+        // constructors => public
+        
+        // WHY
+        // 1. accessor & control issue
+        // 2. internal change won't affect external change (internal name change won't
+        // affect anything outside) => more isolated code
+        // 3. can also perform validation inside setters methods
+        
+    }
+    
+}
