a88c981a057861fcdf839bc5c38b79b5338a5f19
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-01-28 22:32:05 +0100

    Collection :  ArrayList

--- src/com/gemalto/O13/Main.java
+++ src/com/gemalto/O13/Main.java
@@ -1,4 +1,21 @@
 package com.gemalto.O13;
 
 public class Main {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        // testing purpose only
+        //theatre.getSeats();
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken.");
+        }
+
+//        if (theatre.reserveSeat("H11")) {
+//            System.out.println("Please pay");
+//        } else {
+//            System.out.println("Sorry, seat is taken.");
+//        }
+    }
 }
