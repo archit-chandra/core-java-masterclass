376d542e084d585897192b82ccb8fd0d7ae5cadd
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-11 00:46:08 +0100

    single & multiple lines inside lambda expression

--- src/com/gemalto/O17/lambda_expressions/LambdaMain1.java
+++ src/com/gemalto/O17/lambda_expressions/LambdaMain1.java
@@ -1,15 +1,90 @@
 package com.gemalto.O17.lambda_expressions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class LambdaMain1 {
     public static void main(String[] args) {
-        new Thread(new CodeToRun()).start();
+        // original code
+//        new Thread(new CodeToRun()).start();
+
+        // same thing as above but using anonymous class
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the Runnable");
+//            }
+//        }).start();
+
+        // same code using lambda expressions
+        new Thread(() -> System.out.println("Printing from the Runnable Using Lambda: Single Line")).start();
+
+        System.out.println("----------Multiple line codes with Lambda------------");
+        new Thread(() -> {
+            System.out.println("Printing from the Runnable: Multiple Line");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();
+
+        System.out.println("----------Dealing with Employees------------");
+        Employee archit = new Employee("Archit Chandra", 34);
+        Employee ankush = new Employee("Ankush Chandra", 29);
+        Employee simran = new Employee("Simran Narula", 29);
+        Employee snow = new Employee("Snow White", 30);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(archit);
+        employees.add(ankush);
+        employees.add(simran);
+        employees.add(snow);
+
+        // Arranging them by sort via name in ascending order
+
+        // Sorting without Lambdas
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
+        for (Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
+
     }
 }
 
-class CodeToRun implements Runnable {
+class Employee {
+    private String name;
+    private int age;
 
-    @Override
-    public void run() {
-        System.out.println("Printing from the Runnable");
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
+
+//class CodeToRun implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("Printing from the Runnable");
+//    }
+//}
