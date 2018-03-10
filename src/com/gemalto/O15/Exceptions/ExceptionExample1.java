8f087e1a2a2f10c95aa2cd1551f9342dd49561a4
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-10 20:12:44 +0100

    wrong way of catching exception

--- src/com/gemalto/O15/Exceptions/ExceptionExample1.java
+++ src/com/gemalto/O15/Exceptions/ExceptionExample1.java
@@ -1,8 +1,15 @@
 package com.gemalto.O15.Exceptions;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+/**
+ * Shows the following exception handling
+ * InputMismatchException - When character is typed instead of digits
+ * NoSuchElementException - When no element is present (Ctrl + D)
+ * AirthmeticException    - When divide by 0
+ */
 public class ExceptionExample1 {
 
     public static void main(String[] args) {
@@ -11,7 +18,12 @@ public class ExceptionExample1 {
     }
 
     private static int divide() {
-        int x = getInt();
+        int x;
+        try {
+            x = getInt();
+        } catch (NoSuchElementException e) { // catching NoSuchElementException here
+            x = getInt(); // this catch again throws an exception => WRONG WAY
+        }
         int y = getInt();
         System.out.println("x is " + x + ", y is " + y);
         return x / y;
@@ -23,7 +35,7 @@ public class ExceptionExample1 {
         while (true) {
             try {
                 return scanner.nextInt();
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) { // still not catching NoSuchElementException
                 // Go round again inside the loop. Read past the end of the line & input first
                 scanner.nextLine();
                 System.out.println("Please enter the number using only digits 0 - 9");
