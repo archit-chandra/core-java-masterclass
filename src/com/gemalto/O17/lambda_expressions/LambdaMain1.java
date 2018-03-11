6ffbf24fb5fe2b82aa716d2113a9e90a60fe6d27
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-11 02:04:51 +0100

    with/without return statement in lambda

--- src/com/gemalto/O17/lambda_expressions/LambdaMain1.java
+++ src/com/gemalto/O17/lambda_expressions/LambdaMain1.java
@@ -61,6 +61,32 @@ public class LambdaMain1 {
             System.out.println(employee.getName());
         }
 
+        System.out.println("----------Printing Silly String Without Lambda---------------");
+        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        }, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println("sillyString = " + sillyString);
+
+        System.out.println("----------Printing Other Silly String With Lambda---------------");
+        // can also assign as a separate variable as below for clarity
+        //UpperConcat upperConcat = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        //String otherSillyString = doStringStuff(upperConcat, employees.get(2).getName(), employees.get(3).getName());
+
+        // If Lambda contains more than 1 statement, then we need to use return keyword, otherwise no need of return keyword
+        UpperConcat upperConcat = (s1, s2) -> {
+            String result = s1.toUpperCase() + s2.toUpperCase();
+            return result;
+        };
+
+        String otherSillyString = doStringStuff((s1, s2) -> s1.toUpperCase() + s2.toUpperCase(), employees.get(2).getName(), employees.get(3).getName());
+        System.out.println("otherSillyString = " + otherSillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -88,6 +114,10 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
 
 //class CodeToRun implements Runnable {
